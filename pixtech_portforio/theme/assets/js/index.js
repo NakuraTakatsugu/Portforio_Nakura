9cfa3e6ba6768fca940b64229571b1aadb0d7288
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-06-08 16:35:05 +0900

    [update]セクションごとのアニメーションを追加

--- pixtech_portforio/theme/assets/js/index.js
+++ pixtech_portforio/theme/assets/js/index.js
@@ -19,6 +19,48 @@ $(function () {
     $toggleButton.on('click', init);
   }
 
-  changeToggleMenu();
+  function activeSwiper() {
+    const swiper = new Swiper('.mySwiper-gallery', {
+      spaceBetween: 6,
+      slidesPerView: 4,
+    });
+
+    const swiper2 = new Swiper('.mySwiper-slider', {
+      thumbs: {
+        swiper: swiper,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+  function fadeAnime() {
+    const trigger = $('.fadeUpTrigger');
+    $(trigger).each(function () {
+      let elementPosition = $(this).offset().top - 50;
+      let scroll = $(window).scrollTop();
+      let windowHeight = $(window).height();
+      if (scroll >= elementPosition - windowHeight) {
+        $(this).addClass('fadeUp');
+      }
+    });
+  }
+
+  function setEvent() {
+    $(window).on({
+      scroll: fadeAnime,
+      load: fadeAnime,
+    });
+  }
+
+  function init() {
+    setEvent();
+    activeSwiper();
+    changeToggleMenu();
+  }
+
+  init();
   console.log('index.js is loading!!');
 });

4f364e181036c50201476f2523c12c25900571ce
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-06-18 00:35:34 +0900

    [add]contact-pagesの追加

---
+++ pixtech_portforio/theme/contact-confirm.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: contact-confirm
+*/
+?>
+<?php get_header(); ?>
+<div class="page-contact confirm">
+<main class="main">
+  <div class="inner">
+    <div class="headline-wrap">
+      <h3 class="headline moderate">
+        CONTACT
+        <span class="subhead dark">お問い合わせ</span>
+      </h3>
+    </div>
+    <div class="contents-wrap">
+    <?php the_content(); ?>
+    </div>
+  </div>
+</main>
+</div>
+<?php get_footer(); ?>
